0c13e08eaf249da06a0938d3fe67c3456baa72c2
Prateek Chaubey <chaubeyprateek@gmail.com>
2017-12-26 15:50:56 +0530

    land: Add Hardware Properties
    Change-Id: I94a422a8b82188d08e4b5e7cada02eeef68773c3

---
+++ product/hardware_prop.mk
@@ -0,0 +1,8 @@
+# Hardware Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.device.chipset=Qualcomm MSM8937 Snapdragon 430 \
+    ro.device.cpu=Octa-core 1.4 GHz Cortex-A53 \
+    ro.device.gpu=Qualcomm Adreno 505 \
+    ro.device.rear_cam=13 MP \
+    ro.device.front_cam=5 MP \
+    ro.device.screen_res=720 x 1280 pixels
